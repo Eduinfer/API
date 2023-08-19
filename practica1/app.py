118afd24c4aaba1c4164dc948779ded19bb0c17b
Eduinfer03 <eduin_quintero@soy.sena.edu.co>
2023-08-18 21:40:44 -0500

    Actividad_API

---
+++ practica1/app.py
@@ -0,0 +1,28 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    #app.logger.debug('iniciando el debug')
+    #app.logger.info('iniciando info')
+    # app.logger.warning('iniciando a nivel warning')
+    # app.logger.error('iniciando a nivel de error')
+    app.logger.info(f'solicitud en la ruta {request.path}')
+    return "<p>Hello, world! desde pycharm</p>"
+
+@app.route('/saludar/<nombre>')
+def saludar(nombre):
+    app.logger.info(f'solicitud en la ruta {request.path}')
+    return  f'hola {nombre}'
+
+@app.route('/edad/<int:edad>')
+def edad(edad):
+    app.logger.info(f'solicitud en la ruta {request.path}')
+    return f'tu edad es {edad}'
+
+#Rest
+@app.get('/api/user/<user>')
+def user(user):
+    valores = {'user': user}
+    return valores
